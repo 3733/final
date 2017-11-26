94639413e2651e6d2bbc8f124e646951d3f97bc5
Stephanie Racca <srracca@wpi.edu>
2017-11-25 19:43:56 -0500

    I can't get this fucking thing to work.

--- TeamF-0.1/src/sample/AutoCompleteTextField.java
+++ TeamF-0.1/src/sample/AutoCompleteTextField.java
@@ -20,94 +20,19 @@ import javafx.collections.ObservableList;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
-/**
- * This class is a TextField which implements an "autocomplete" functionality,
- * based on a supplied list of entries.<p>
- *
- * If the entered text matches a part of any of the supplied entries these are
- * going to be displayed in a popup. Further the matching part of the entry is
- * going to be displayed in a special style, defined by
- * {@link #textOccurenceStyle textOccurenceStyle}. The maximum number of
- * displayed entries in the popup is defined by
- * {@link #maxEntries maxEntries}.<br>
- * By default the pattern matching is not case-sensitive. This behaviour is
- * defined by the {@link #caseSensitive caseSensitive}
- * .<p>
- *
- * The AutoCompleteTextField also has a List of
- * {@link #filteredEntries filteredEntries} that is equal to the search results
- * if search results are not empty, or {@link #filteredEntries filteredEntries}
- * is equal to {@link #entries entries} otherwise. If
- * {@link #popupHidden popupHidden} is set to true no popup is going to be
- * shown. This list can be used to bind all entries to another node (a ListView
- * for example) in the following way:
- * <pre>
- * <code>
- * AutoCompleteTextField auto = new AutoCompleteTextField(entries);
- * auto.setPopupHidden(true);
- * SimpleListProperty filteredEntries = new SimpleListProperty(auto.getFilteredEntries());
- * listView.itemsProperty().bind(filteredEntries);
- * </code>
- * </pre>
- *
- * @author Caleb Brinkman
- * @author Fabian Ochmann
- */
 public class AutoCompleteTextField extends TextField {
 
-    /**
-     * The existing autocomplete entries.
-     */
+
     private final SortedSet<String> entries;
 
-    /**
-     * The set of filtered entries:<br>
-     * Equal to the search results if search results are not empty, equal to
-     * {@link #entries entries} otherwise.
-     */
     private ObservableList<String> filteredEntries
             = FXCollections.observableArrayList();
-
-    /**
-     * The popup used to select an entry.
-     */
     private ContextMenu entriesPopup;
-
-    /**
-     * Indicates whether the search is case sensitive or not. <br>
-     * Default: false
-     */
     private boolean caseSensitive = false;
-
-    /**
-     * Indicates whether the Popup should be hidden or displayed. Use this if
-     * you want to filter an existing list/set (for example values of a
-     * {@link javafx.scene.control.ListView ListView}). Do this by binding
-     * {@link #getFilteredEntries() getFilteredEntries()} to the list/set.
-     */
     private boolean popupHidden = false;
-
-    /**
-     * The CSS style that should be applied on the parts in the popup that match
-     * the entered text. <br>
-     * Default: "-fx-font-weight: bold; -fx-fill: red;"
-     * <p>
-     * Note: This style is going to be applied on an
-     * {@link javafx.scene.text.Text Text} instance. See the <i>JavaFX CSS
-     * Reference Guide</i> for available CSS Propeties.
-     */
     private String textOccurenceStyle = "-fx-font-weight: bold; "
             + "-fx-fill: red;";
-
-    /**
-     * The maximum Number of entries displayed in the popup.<br>
-     * Default: 10
-     */
     private int maxEntries = 10;
-
-    /**
-     * Construct a new AutoCompleteTextField.
-     */
     public AutoCompleteTextField(SortedSet<String> entrySet) {
         super();
         this.entries = (entrySet == null ? new TreeSet<String>() : entrySet);
@@ -168,21 +93,10 @@ public class AutoCompleteTextField extends TextField {
 
     }
 
-    /**
-     * Get the existing set of autocomplete entries.
-     *
-     * @return The existing autocomplete entries.
-     */
     public SortedSet<String> getEntries() {
         return entries;
     }
-
-    /**
-     * Populate the entry set with the given search results. Display is limited
-     * to 10 entries, for performance.
-     *
-     * @param searchResult The set of matching strings.
-     */
+    
     private void populatePopup(List<String> searchResult, String text) {
         List<CustomMenuItem> menuItems = new LinkedList<>();
         int count = Math.min(searchResult.size(), getMaxEntries());
