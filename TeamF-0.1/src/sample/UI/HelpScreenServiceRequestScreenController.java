4500d5d858869ae8be6429f57526ff2d611244b5
William Godsey <willgodsey@gmail.com>
2017-12-03 17:09:06 -0500

    Hooray for controllers

---
+++ TeamF-0.1/src/sample/UI/HelpScreenServiceRequestScreenController.java
@@ -0,0 +1,14 @@
+package sample.UI;
+
+import javafx.fxml.FXML;
+import sample.Main;
+
+public class HelpScreenServiceRequestScreenController {
+    private Main mainController;
+    public void setMainController(Main main){ this.mainController = main;}
+
+    @FXML
+    public void back(){Main.startScreen();}
+    
+
+}
