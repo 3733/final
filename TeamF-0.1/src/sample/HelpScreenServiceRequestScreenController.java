bb796f5b11120526f19015e5cd687748ee1371bf
William Godsey <willgodsey@gmail.com>
2017-12-04 21:03:49 -0500

    code is good

--- TeamF-0.1/src/sample/HelpScreenServiceRequestScreenController.java
+++ TeamF-0.1/src/sample/HelpScreenServiceRequestScreenController.java
@@ -1,5 +1,6 @@
 package sample;
 
+import com.jfoenix.controls.JFXTextArea;
 import javafx.fxml.FXML;
 import sample.Main;
 
@@ -13,6 +14,24 @@ public class HelpScreenServiceRequestScreenController {
     @FXML
     public void help(){Main.setHelpScreenServiceRequestScreen();}
 
+    Node n1 = new Node("FDEPT00101", 1614, 829, "1", "Tower", "DEPT", "Center for International Medecine", "CIM", 'F');
+    @FXML
+    public JFXTextArea box;
+
+    public int id = 9000;
 
 
+    @FXML
+    public void sendITHelpRequest() throws MissingFieldException { //when the Send button is pressed
+        ItRequest newIt = new ItRequest(n1 , box.getText(),
+                id, "", "",
+                "", 0000, "it", "unaccepted",
+               1);
+
+        //requestList.add(newAssist);               //new service request is made and added to priority queue
+        testEmbeddedDB.addItRequest(newIt);
+
+        box.clear();
+        id++;
+    }
 }
