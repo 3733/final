d2da499496e281af8c47a38a63d7c1c779501e6e
8bitmime <bmmattiuzzi@wpi.edu>
2017-12-05 18:47:24 +0000

    another day another commit message

--- TeamF-0.1/src/sample/MessengerController.java
+++ TeamF-0.1/src/sample/MessengerController.java
@@ -3,6 +3,7 @@ package sample;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXListView;
 import com.jfoenix.controls.JFXTextField;
+import dev2dev.textclient.TextClient;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.fxml.Initializable;
@@ -22,10 +23,12 @@ public class MessengerController implements Initializable{
 
     private Main mainController;
 
+    TextClient t = new TextClient();
 
     //Purpose: to initialize the fxml
     @Override
     public void initialize(URL location, ResourceBundle resources){
+        t.boot("bmat2z", 6969);
 
     }
 
@@ -33,6 +36,11 @@ public class MessengerController implements Initializable{
     //getters and setters
     public void setMainController(Main in){
         mainController = in;
+    }
+
+    //send button
+    public void send(){
+        t.sendMsg(fromLabel.getText(), questionAsked.getText());
     }
 
 }
